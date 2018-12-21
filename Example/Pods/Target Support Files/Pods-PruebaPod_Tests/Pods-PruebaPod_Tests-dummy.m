fd61194fffbe1b97166b16f86187b81a82a35f99
Manu <manu.stier@me.com>
2018-12-21 12:19:55 +0100

    Create Project

---
+++ Example/Pods/Target Support Files/Pods-PruebaPod_Tests/Pods-PruebaPod_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PruebaPod_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PruebaPod_Tests
+@end
---
+++ Example/Pods/Target Support Files/PruebaPod/PruebaPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PruebaPod : NSObject
+@end
+@implementation PodsDummy_PruebaPod
+@end
